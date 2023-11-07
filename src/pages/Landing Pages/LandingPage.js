f28ab26a2c9bd5ec791f3b8a7b1aef2e3274edab
jethro.libutan <jethrolibutan@ciinc.com>
2023-11-07 00:15:04 -0500

    gave landing page styling and starter text

--- src/pages/Landing Pages/LandingPage.js
+++ src/pages/Landing Pages/LandingPage.js
@@ -1,11 +1,36 @@
 import React from "react";
 import "./LandingPage.css";
+import { Button } from "@mui/material";
+import { Link } from "react-router-dom";
 
 function LandingPage() {
   return (
     <div className="landing-page">
-      {" "}
-      <h1>Welcome to Contractor Control</h1>
+      <div className="landing-page-column">
+        <h1 className="landing-page-header">Welcome to Contractor Control!</h1>
+        <h2 className="landing-page-subheader">
+          The one stop shop for all your contractor needs!
+        </h2>
+        <Link to="/login">
+          <Button
+            sx={{
+              borderRadius: "50px",
+              borderWidth: "2px",
+              borderColor: "blue",
+              borderStyle: "solid",
+              backgroundColor: "blue",
+              color: "white",
+              "&:hover": {
+                backgroundColor: "darkblue",
+                borderColor: "darkblue",
+              },
+            }}
+            variant="contained"
+          >
+            Click here to get started!
+          </Button>
+        </Link>
+      </div>
     </div>
   );
 }
