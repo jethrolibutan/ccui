327b3e3c663c23f984eadaa4381966d5a9045836
jethrolibutan <jethrolibutan@gmail.com>
2023-11-07 17:28:20 -0500

    created invoice form

--- src/pages/Functional Pages/Invoice.js
+++ src/pages/Functional Pages/Invoice.js
@@ -1,7 +1,13 @@
 import React from "react";
+import InvoiceForm from "../../components/Invoice/InvoiceForm";
 
 function Invoice() {
-  return <div>this is the invoice page</div>;
+  return (
+    <div>
+      this is the invoice page
+      <InvoiceForm />
+    </div>
+  );
 }
 
 export default Invoice;
