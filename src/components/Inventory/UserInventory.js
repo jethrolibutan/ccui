40edb48db9b3bac2912bd5e13a4bde912ab08df9
jethrolibutan <jethrolibutan@gmail.com>
2023-11-16 11:31:03 -0500

    saving progress towards updating quantity

--- src/components/Inventory/UserInventory.js
+++ src/components/Inventory/UserInventory.js
@@ -8,15 +8,38 @@ import {
   TableRow,
   Paper,
   Button,
+  Modal,
+  Box,
+  TextField,
 } from "@mui/material";
+import * as Yup from "yup";
+import { useFormik } from "formik";
 import { useInventory } from "../../contexts/InventoryContext";
 import "./UserInventory.css";
 import axios from "axios";
 
 function UserInventory() {
-  const { inventory } = useInventory();
+  const { inventory, deleteItem } = useInventory();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedItemId, setSelectedItemId] = useState(null);
 
-  // console.log("this is the inventory", inventory);
+  const formik = useFormik({
+    initialValues: {
+      newQuantity: "",
+    },
+    validationSchema: Yup.object({
+      newQuantity: Yup.number()
+        .typeError("Must be a number")
+        .required("Required")
+        .positive("Must be a positive number")
+        .integer("Must be an integer"),
+    }),
+    onSubmit: (values) => {
+      console.log("Form submitted with values:", values);
+      handleUpdateQuantity(values.newQuantity);
+      console.log("new quantity", values.newQuantity);
+    },
+  });
 
   const handleDelete = async (itemId) => {
     console.log(itemId);
@@ -27,7 +50,13 @@ function UserInventory() {
         item_id: itemId,
       })
       .then((response) => {
-        console.log(response);
+        if (response.status === 200) {
+          // Call deleteItem from the context to update the state
+          deleteItem(itemId);
+          console.log("Item deleted successfully.");
+        } else {
+          console.log("Failed to delete item.");
+        }
       })
 
       .catch((error) => {
@@ -35,7 +64,42 @@ function UserInventory() {
       });
   };
 
-  const updateQuantity = (itemId) => {};
+  const handleUpdateQuantity = async () => {
+    try {
+      // Your update quantity logic
+      const response = await axios.post(
+        `http://localhost:8000/api/update-item-quantity/`,
+        {
+          jwt: localStorage.getItem("jwt"),
+          item_id: selectedItemId,
+          new_quantity: formik.values.newQuantity,
+        }
+      );
+
+      // Check if the update quantity request was successful
+      if (response.status === 200) {
+        // Close the modal and reset states
+
+        // Close the modal and reset states
+        setIsModalOpen(false);
+        setSelectedItemId(null);
+
+        // Manually reset the Formik form
+        formik.resetForm();
+
+        // Enable the "Update Quantity" button
+        setIsModalOpen(false);
+        setSelectedItemId(null);
+
+        console.log("Quantity updated successfully.");
+      } else {
+        console.log("Failed to update quantity.");
+        formik.setSubmitting(false);
+      }
+    } catch (error) {
+      console.error("Error updating quantity:", error);
+    }
+  };
 
   const renderTableRows = () => {
     return inventory.map((item) => {
@@ -53,7 +117,10 @@ function UserInventory() {
               Delete
             </Button>
             <Button
-              onClick={() => handleDelete(item.id)}
+              onClick={() => {
+                setIsModalOpen(true);
+                setSelectedItemId(item.id);
+              }}
               variant="outlined"
               color="secondary"
             >
@@ -64,6 +131,8 @@ function UserInventory() {
       );
     });
   };
+
+  console.log("new quantity", formik.values.newQuantity);
 
   return (
     <div className="inventory-section">
@@ -79,6 +148,48 @@ function UserInventory() {
           <TableBody>{renderTableRows()}</TableBody>
         </Table>
       </TableContainer>
+      <Modal open={isModalOpen} onClose={() => setIsModalOpen(false)}>
+        <Box
+          sx={{
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            width: 400,
+            bgcolor: "background.paper",
+            boxShadow: 24,
+            p: 4,
+          }}
+        >
+          <form onSubmit={formik.handleSubmit}>
+            <TextField
+              label="New Quantity"
+              variant="outlined"
+              fullWidth
+              type="number"
+              id="newQuantity"
+              name="newQuantity"
+              value={formik.values.newQuantity}
+              onChange={formik.handleChange}
+              error={
+                formik.touched.newQuantity && Boolean(formik.errors.newQuantity)
+              }
+              helperText={
+                formik.touched.newQuantity && formik.errors.newQuantity
+              }
+            />
+            <Button
+              type="submit"
+              variant="outlined"
+              color="primary"
+              sx={{ mt: 2 }}
+              // disabled={formik.isSubmitting || !formik.isValid}
+            >
+              Update Quantity
+            </Button>
+          </form>
+        </Box>
+      </Modal>
     </div>
   );
 }
